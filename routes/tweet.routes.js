9a115d87914f4ec13344c4fa5c527884f6825e5f
Ewerton da Costa Vaz <ewerton.vaz@gmail.com>
2022-12-27 11:05:01 -0300

    concluida a implementação de pagination na rota GET /tweets

--- routes/tweet.routes.js
+++ routes/tweet.routes.js
@@ -10,21 +10,47 @@ const serverAddr = {
 
 tweetRoutes.get("/", async (req, res) => {
     const { page, perpage} = req.query;
-    const pageNum = page ? page : 1;
-    const pageLimit = perpage ? perpage : 10;
+    const pageNum = page ? +page : 1;
+    const pageLimit = perpage ? +perpage : 10;
+
     try {
-      const tweets = await TweetModel
+      const baseUrl = req.protocol + '://' + req.get('host') + '/tweets';
+      const numPages = Math.ceil( await TweetModel.count() / pageLimit );
+      const navigator = {
+        numpages: numPages,
+        current: pageNum,
+        perpage: pageLimit,
+        url: {
+          current:`${baseUrl}?page=${pageNum}&perpage=${pageLimit}`,
+          previous: pageNum > 1 ? `${baseUrl}?page=${pageNum - 1}&perpage=${pageLimit}` : null,
+          next: pageNum < numPages ? `${baseUrl}?page=${pageNum + 1}&perpage=${pageLimit}` : null,
+          first: `${baseUrl}?page=1&perpage=${pageLimit}`,
+          last: `${baseUrl}?page=${numPages}&perpage=${pageLimit}`
+        }
+      }
+      const data = await TweetModel
         .find({})
         .skip( (pageNum - 1) * pageLimit)
         .limit(pageLimit)
         .populate("user", ["name", "email", "username", "profilePic"]);
   
-      return res.status(200).json(tweets);
+      return res.status(200).json({navigator, data});
     } catch (error) {
       console.log(error);
       return res.status(500).json(error.errors);
     }
 });
 
-
+tweetRoutes.get("/:tweetId", async (req, res) => {
+  const { tweetId } = req.params;
+  try {
+    const tweet = await TweetModel
+      .findById( tweetId )
+      .populate("user", ["name", "email", "username", "profilePic"]);
+    return res.status(200).json(tweet);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
 export default tweetRoutes;
